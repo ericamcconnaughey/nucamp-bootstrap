c09a06d5e2fcb8aa1f0977215dbcf8564592f922
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-07 13:05:23 -0600

    fix play/pause button on carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(function () {
+  //carousel controls
   $(".carousel").carousel({ interval: 2000 });
   $("#carouselButton").click(function () {
     if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-      $(".carousel").carousel(pause);
+      $(".carousel").carousel('pause');
       $("#carouselButton").children("i").removeClass("fa-pause");
       $("#carouselButton").children("i").addClass("fa-play");
     } else {
@@ -11,9 +12,11 @@ $(function () {
       $("#carouselButton").children("i").addClass("fa-pause");
     }
   });
+  //reserve button
   $("#reserveButton").click(function () {
     $("#reserveModal").modal("show");
   });
+  //login button
   $("#loginButton").click(function () {
     $("#loginModal").modal("show");
   })
